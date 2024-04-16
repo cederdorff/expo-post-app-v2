0cad7ada43eab9279ae3f7bd492946ae75634d46
RACE (Rasmus Cederdorff) <rasmus@cederdorff.com>
2024-04-16 11:04:42 +0200

    Add @expo/react-native-action-sheet dependency and update other dependencies

---
+++ app/(tabs)/index.jsx
@@ -0,0 +1,90 @@
+import { Stack, useFocusEffect, useRouter } from "expo-router";
+import { useCallback, useEffect, useState } from "react";
+import {
+  Button,
+  FlatList,
+  RefreshControl,
+  StyleSheet,
+  View
+} from "react-native";
+import Post from "../components/Post";
+
+export default function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
+  const router = useRouter();
+
+  const API_URL = "https://expo-post-app-default-rtdb.firebaseio.com";
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
+  // Sometimes we want to run side-effects when a screen is focused.
+  // https://reactnavigation.org/docs/use-focus-effect/
+  useFocusEffect(
+    // If you don't wrap your effect in React.useCallback, the effect will run every render if the screen is focused.
+    useCallback(() => {
+      getPosts();
+    }, [])
+  );
+
+  async function getPosts() {
+    const response = await fetch(`${API_URL}/posts.json`);
+    const dataObj = await response.json();
+    const postsArray = Object.keys(dataObj).map(key => ({
+      id: key,
+      ...dataObj[key]
+    })); // from object to array
+    postsArray.sort((postA, postB) => postB.createdAt - postA.createdAt); // sort by timestamp/ createdBy
+    setPosts(postsArray);
+  }
+
+  function renderPost(item) {
+    const post = item.item;
+    return <Post post={post} reload={getPosts} />;
+  }
+
+  async function handleRefresh() {
+    setRefreshing(true);
+    await getPosts();
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 500);
+  }
+
+  return (
+    <View style={styles.list}>
+      <Stack.Screen
+        options={{
+          headerRight: () => (
+            <Button
+              title="Add New"
+              color="#fff"
+              onPress={() => router.push("/post-modal")}
+            />
+          )
+        }}
+      />
+
+      <FlatList
+        data={posts}
+        renderItem={renderPost}
+        keyExtractor={post => post.id}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+            tintColor="#264c59"
+          />
+        }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1
+  }
+});
---
+++ app/components/MapMarker.js
@@ -0,0 +1,36 @@
+// import { useRouter } from "expo-router";
+// import { Image, StyleSheet, Text, View } from "react-native";
+// import { Callout, Marker } from "react-native-maps";
+// export default function MapMarker({ post }) {
+//     const router = useRouter();
+
+//     function handleCalloutPress(id) {
+//         router.push(`map/${id}`);
+//     }
+
+//     return (
+//         <Marker coordinate={post.location}>
+//             <Callout onPress={() => handleCalloutPress(post.id)}>
+//                 <View style={styles.calloutView}>
+//                     <Text style={styles.caption}>{post.caption}</Text>
+//                     <Image
+//                         source={{ uri: post.image }}
+//                         style={styles.image}
+//                     />
+//                 </View>
+//             </Callout>
+//         </Marker>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+//     calloutView: {
+//         flex: 1
+//     },
+//     image: { height: 100 },
+//     caption: {
+//         fontSize: 14,
+//         fontWeight: "bold",
+//         marginBottom: 10
+//     }
+// });
